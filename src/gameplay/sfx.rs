629b864245f6ca7d22910337cf0cb4f4d3511047
Corvus <corvusprudens@gmail.com>
2026-04-02 11:24:41 -0600

    a

--- src/gameplay/sfx.rs
+++ src/gameplay/sfx.rs
@@ -6,11 +6,12 @@ use bevy_seedling::{
     prelude::Volume,
     sample::{AudioSample, RandomPitch, SamplePlayer},
 };
+use rand::{Rng, SeedableRng};
 
 use crate::gameplay::player::Player;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_systems(PostUpdate, walking)
+    app.add_systems(PostUpdate, (walking, hello))
         .add_systems(
             Startup,
             (
@@ -81,5 +82,35 @@ fn jumping(
             SamplePlayer::new(server.load("sfx/squeak.ogg")).with_volume(Volume::Decibels(3.0)),
             RandomPitch(1.0..1.5),
         ));
+    }
+}
+
+struct HelloTimer(Timer);
+
+impl Default for HelloTimer {
+    fn default() -> Self {
+        Self(Timer::from_seconds(1.0, TimerMode::Repeating))
+    }
+}
+
+struct HelloRng(rand::rngs::SmallRng);
+
+impl Default for HelloRng {
+    fn default() -> Self {
+        Self(rand::rngs::SmallRng::from_os_rng())
+    }
+}
+
+fn hello(
+    time: Res<Time>,
+    server: Res<AssetServer>,
+    mut timer: Local<HelloTimer>,
+    mut rng: Local<HelloRng>,
+    mut commands: Commands,
+) {
+    if timer.0.tick(time.delta()).just_finished() && rng.0.random_range(0..1000) == 0 {
+        commands.spawn(
+            SamplePlayer::new(server.load("sfx/hello.ogg")).with_volume(Volume::Decibels(-24.0)),
+        );
     }
 }
