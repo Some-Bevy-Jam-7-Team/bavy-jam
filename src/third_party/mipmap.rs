d892ab986e1a9de6041a26be6e00a1b09a704be1
Daniel Gallups <dsgallups@protonmail.com>
2026-04-02 13:41:04 -0400

    fafo

--- src/third_party/mipmap.rs
+++ src/third_party/mipmap.rs
@@ -1,7 +1,13 @@
 use bevy::prelude::*;
-use bevy_mod_mipmap_generator::{MipmapGeneratorPlugin, generate_mipmaps};
+use bevy_mod_mipmap_generator::{MipmapGeneratorPlugin, MipmapGeneratorSettings, generate_mipmaps};
+use image::imageops::FilterType;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins(MipmapGeneratorPlugin)
+        .insert_resource(MipmapGeneratorSettings {
+            anisotropic_filtering: 16,
+            filter_type: FilterType::Gaussian,
+            ..Default::default()
+        })
         .add_systems(Update, generate_mipmaps::<StandardMaterial>);
 }
