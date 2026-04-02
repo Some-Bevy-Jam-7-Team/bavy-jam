9aa9acc4bbbb908de3c5e0f355f6c9e2641fc544
Daniel Gallups <dsgallups@protonmail.com>
2026-04-02 11:28:29 -0400

    init sfx module

--- src/gameplay/mod.rs
+++ src/gameplay/mod.rs
@@ -1,7 +1,8 @@
 use bevy::prelude::*;
 mod npc;
 mod player;
+mod sfx;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins((player::plugin, npc::plugin));
+    app.add_plugins((player::plugin, npc::plugin, sfx::plugin));
 }
